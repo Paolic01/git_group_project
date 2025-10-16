a09b51a191973480b86e754e8eb5f6c3f934c137
Pasquale <pasquale.paolicelli@gmail.com>
2025-10-16 17:13:41 +0200

    first commit

---
+++ group.py
@@ -0,0 +1,11 @@
+def pari_o_dispari(numero):
+    if numero % 2 == 0:
+        return "pari"
+    else:
+        return "dispari"
+
+if __name__ == "__main__":
+    numero = int(input("Inserisci un numero: "))
+    print(f"Il numero {numero} è {pari_o_dispari(numero)}")
+
+    
