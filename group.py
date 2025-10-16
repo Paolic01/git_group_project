7b86ccbd9743203048e391bb2633362f452ecb17
AB.Orpheo <xorpheo.dev@gmail.com>
2025-10-16 17:42:09 +0200

    Aggiunto file txt

--- group.py
+++ group.py
@@ -8,5 +8,3 @@ def pari_o_dispari(numero):
 if __name__ == "__main__":
     numero = int(input("Inserisci un numero: "))
     print(f"Il numero {numero} è {pari_o_dispari(numero)}")
-
-print("\nModifica di Abdellah")
