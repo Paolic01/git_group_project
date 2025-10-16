bc3a885d665d527b7de733753d9f0c4fa58d0e09
Hflavio12 <flaviogioia.fg@gmail.com>
2025-10-16 17:28:42 +0200

    Aggiunto un nuovo file chiamato group2.py

---
+++ group2.py
@@ -0,0 +1,12 @@
+# 
+def dispari_o_pari(numero):
+    if numero % 2 == 0:
+        return "pari"
+    else:
+        return "dispari"
+
+if __name__ == "__main__":
+    numero = int(input("Inserisci un numero: "))
+    print(f"Il numero {numero} è {pari_o_dispari(numero)}")
+
+    
